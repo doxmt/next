c0e0f2f69de2cae8d4b223d266919221ce3e2177
Songtaeha <songtaeha@naver.com>
2025-10-07 17:45:54 +0900

    UI 구현 + Moc 데이터

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,16 +1,18 @@
-import ClientComponent from "@/components/client-component";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
-export default async function Page(props: {
+export default async function Page({
+  searchParams,
+}: {
   searchParams: Promise<{ q?: string }>;
 }) {
-  const searchParams = await props.searchParams;
+  const { q } = await searchParams;
 
   return (
     <div>
-      Search 페이지 : {searchParams.q}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
--- section03/src/components/server-component.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-
-export default function Servercomponent({ children }: { children: ReactNode }) {
-  console.log("서버 컴포넌트");
-  return <div>{children}</div>;
-}
